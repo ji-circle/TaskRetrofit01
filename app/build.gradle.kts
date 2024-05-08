00f4f1337d884e8b480736b275aebb382fc892ec
ji-circle <sp202401jw@gmail.com>
2024-05-09 08:56:34 +0900

    옮기는중

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.taskretrofit01"
     compileSdk = 34
 
+    buildFeatures{
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.taskretrofit01"
         minSdk = 31
